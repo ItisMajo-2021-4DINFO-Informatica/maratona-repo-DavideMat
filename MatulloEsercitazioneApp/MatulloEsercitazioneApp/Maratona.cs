1847ed44e402bcfcd61965cfc490e019fa3451b9
DavideMat <98027161+DavideMat@users.noreply.github.com>
2022-02-16 13:53:19 +0100

    Maratona

---
+++ MatulloEsercitazioneApp/MatulloEsercitazioneApp/Maratona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatulloEsercitazioneApp
+{
+    class Maratona
+    {
+        public string NomeAtleta { get; set; }
+        public string Società { get; set; }
+        public string Tempo { get; set; }
+        public string Città { get; set; }
+    }
+}
---
+++ MatulloEsercitazioneApp/MatulloEsercitazioneApp/Maratone.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace MatulloEsercitazioneApp
+{
+    class Maratone
+    {
+        public List<Maratona> ElencoMaratone;
+
+
+        public Maratone()
+        {
+
+            ElencoMaratone = new List<Maratona>();
+        }
+
+        public void Aggiungi(Maratona maratona)
+        {
+            ElencoMaratone.Add(maratona);
+        }
+
+        public void LeggiDati()
+        {
+
+            using (FileStream flussoDati = new FileStream("Maratone.txt", FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader lettoreDati = new StreamReader(flussoDati))
+                {
+                    while (!lettoreDati.EndOfStream)
+                    {
+                        
+                        string linea = lettoreDati.ReadLine();
+                        string[] campi = linea.Split('%');
+
+                        Maratona maratona = new Maratona();
+                        maratona.NomeAtleta = campi[0];
+                        maratona.Società = campi[1];
+                        maratona.Tempo = campi[2];
+                        maratona.Città = campi[3];
+
+                        Aggiungi(maratona);
+
+
+
+                    }
+
+
+                }
+            }
+
+
+        }
+
+
+        public 
+
+
+
+
+
+
+
+
+
+
+    }
+}
